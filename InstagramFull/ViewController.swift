4e239a0440d6775ece272e13e18c8337811c1ce1
Iftiquar Ahmed <ove@Iftiquars-MacBook-Pro.local>
2019-02-11 16:53:17 +0600

    first commit

---
+++ InstagramFull/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFull
+//
+//  Created by Iftiquar Ahmed  on 2/11/19.
+//  Copyright © 2019 Iftiquar Ahmed . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
